197b7ad283b9b44260272090b9cde4b0ebfccba4
Paulo Sandino <paulosandino@hotmail.com>
2021-02-16 16:49:35 -0300

    Changed the function to calculate area

--- main.c
+++ main.c
@@ -2,28 +2,48 @@
 #include <stdlib.h>
 #include <math.h>
 #include <pthread.h>
+#include <unistd.h>
 
-float a = 0.0;
-float b = 2.0 * M_PI;
-float total_area = 0.0;
-float height = 0.0;
+double a = 1;
+double b = 5;
+int number_of_trapezoids;
+int number_of_threads;
 
-float function_1(float x);
+int is_non_integer_division = 0;
+double *thread_return_array;
 
-float function_2(float x);
+double f1(double x);
 
-void *hello_world(void *thread_parameter)
+void *calculate_area(void *params)
 {
-    int trapezoids_per_thread = (int)(size_t)thread_parameter;
+    int thread_number = (int)(size_t)params;
+
+    double height = (b - a) / number_of_trapezoids;
+    int trapezoids_per_thread = number_of_trapezoids / number_of_threads;
+
+    double local_a = a + (thread_number * trapezoids_per_thread * height);
+    double local_b = local_a + trapezoids_per_thread * height;
+
+    printf("Thread %i local a: %f \n", thread_number, local_a);
+    printf("Thread %i local b: %f \n", thread_number, local_b);
 
     printf("Trapézios na thread: %i \n", trapezoids_per_thread);
 
+    double total_area = (f1(local_a) + f1(local_b)) / 2;
+
     for (int i = 1; i < trapezoids_per_thread; i++)
     {
-        float x_i = a + i * height;
-        total_area += function_2(x_i);
+        double x_i = local_a + i * height;
+        total_area += f1(x_i);
     }
+
     total_area = height * total_area;
+
+    printf("Total area: %f \n", total_area);
+
+    thread_return_array[thread_number] = total_area;
+
+    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
@@ -33,8 +53,8 @@ int main(int argc, char *argv[])
 
     printf("\n");
 
-    int number_of_threads = atoi(argv[1]);
-    int number_of_trapezoids = atoi(argv[2]);
+    number_of_threads = atoi(argv[1]);
+    number_of_trapezoids = atoi(argv[2]);
 
     // Validate inputs
 
@@ -42,46 +62,47 @@ int main(int argc, char *argv[])
     int status;
     void *thread_return;
 
-    int trapezoids_per_thread = number_of_trapezoids / number_of_threads;
-
-    int module = number_of_trapezoids % number_of_threads;
-
-    height = (b - a) / number_of_trapezoids;
-
-    total_area = (function_2(a) + function_2(b)) / 2;
+    is_non_integer_division = number_of_trapezoids % number_of_threads;
+    thread_return_array = malloc(sizeof(double) * number_of_threads);
 
     for (int i = 0; i < number_of_threads; i++)
     {
-        printf("Criando thread #%i \n", i + 1);
-        status = pthread_create(&threads[i], NULL, hello_world, (module == 1 && i == 0) ? (void *)(size_t)trapezoids_per_thread + 1 : (void *)(size_t)trapezoids_per_thread);
+        printf("Criando thread #%i \n", i);
+        status = pthread_create(&threads[i], NULL, calculate_area, (void *)(size_t)i);
 
         if (status != 0)
         {
             printf("Erro na criação da thread. Codigo de erro: %i \n", status);
             return 1;
         }
+        sleep(0.5);
     }
 
     for (int i = 0; i < number_of_threads; i++)
     {
-        printf("Esperando thread #%i finalizar \n", i + 1);
+        printf("Esperando thread #%i finalizar \n", i);
         pthread_join(threads[i], &thread_return);
-        printf("Thread #%i finalizada \n", i + 1);
+        printf("Retorno da thread %i: %f \n", i, thread_return_array[i]);
+        printf("Thread #%i finalizada \n", i);
     }
 
     printf("\n");
 
-    printf("Area da funcao: %.2e \n", total_area);
+    double sum_of_areas = 0.0;
+
+    for (int i = 0; i < number_of_threads; i++)
+    {
+        sum_of_areas += thread_return_array[i];
+    }
+
+    printf("Area da funcao: %.26f \n", sum_of_areas);
+
+    free(thread_return_array);
 
     return 0;
 }
 
-float function_1(float x)
+double f1(double x)
 {
-    return 5.0;
-}
-
-float function_2(float x)
-{
-    return sin(2.0 * x) + cos(5.0 * x);
+    return log10(x);
 }
---
+++ readme.md
@@ -0,0 +1,6 @@
+f1: 
+    f(x) = 2x - 1; 
+    a = 1; 
+    b = 5;
+    real result: 1.7577
+    result by trapezoidal rule: 
